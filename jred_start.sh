b33ba4c41aefb1035171c7a6af11ac566cb8d12e
Igor Demura <igor.demura@gmail.com>
2019-10-22 00:45:32 -0700

    Update scripts

--- jred_start.sh
+++ jred_start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
 mkdir -p $HOME/.jred
-nohup java -XX:+UseG1GC -jar $(dirname $0)/${LOCAL}jred-all.jar "$@" server \
+nohup java -XX:+UseG1GC -jar $(dirname $0)/${JRED_PATH}jred-all.jar "$@" server \
     < /dev/null \
     > $HOME/.jred/server.log 2>&1 &
